9c2dd6facf60801779cf5eb6ac982235f042f73b
Jean Luca <jeanlucacruz44@gmail.com>
2023-12-18 12:15:56 -0300

    atividade

--- recados/js/scripts.js
+++ recados/js/scripts.js
@@ -1,39 +1,39 @@
-const messagesContainer = document.querySelector('.messages-list')
-
-const prevPage = document.getElementById('prevPage')
-const nextPage = document.getElementById('nextPage')
+const messagesContainer = document.querySelector('.messages-list');
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
+const currentPageInput = document.getElementById('currentPage');
+const goToPageBtn = document.getElementById('goToPage');
 
 // Variáveis globais
-let currentPage = 1
-let totalPages = 1
+let currentPage = 1;
+let totalPages = 1;
 
 async function fetchMessages(page) {
   try {
-    const userId = localStorage.getItem('userId')
+    const userId = localStorage.getItem('userId');
 
     if (!userId) {
-      alert("Você precisa fazer login para visualizar os recados.")
-
-      return
+      alert("Você precisa fazer login para visualizar os recados.");
+      return;
     }
 
     const params = {
       page,
       perPage: 3
-    }
+    };
 
-    const response = await api.get(`/notes/${userId}`, { params })
-    const messages = response.data.userMessages
+    const response = await api.get(`/notes/${userId}`, { params });
+    const messages = response.data.userMessages;
 
-    console.log(messages)
+    console.log(messages);
 
-    totalPages = response.data.totalPages
+    totalPages = response.data.totalPages;
 
-    messagesContainer.innerHTML = ''
+    messagesContainer.innerHTML = '';
 
     messages.forEach(message => {
-      const messageCard = document.createElement('div')
-      messageCard.classList.add('card')
+      const messageCard = document.createElement('div');
+      messageCard.classList.add('card');
 
       messageCard.innerHTML = `
         <h2 class="card-title">${message.title}</h2>
@@ -42,52 +42,62 @@ async function fetchMessages(page) {
           <i class="fas fa-solid fa-trash" data-id=${message.id}></i>
           <i class="fas fa-regular fa-edit" data-id=${message.id}></i>
         </div>
-      `
+      `;
 
-      messagesContainer.appendChild(messageCard)
+      messagesContainer.appendChild(messageCard);
 
-      const deleteIcon = messageCard.querySelector('.fa-trash')
+      const deleteIcon = messageCard.querySelector('.fa-trash');
 
       deleteIcon.addEventListener('click', () => {
-        const messageId = deleteIcon.getAttribute('data-id')
+        const messageId = deleteIcon.getAttribute('data-id');
+        deleteMessage(messageId);
+      });
 
-        deleteMessage(messageId)
-      })
-
-      const editIcon = messageCard.querySelector('.fa-edit')
+      const editIcon = messageCard.querySelector('.fa-edit');
       editIcon.addEventListener('click', () => {
-        const messageId = editIcon.getAttribute('data-id')
-
-        navigateToEditPage(messageId)
-      })
+        const messageId = editIcon.getAttribute('data-id');
+        navigateToEditPage(messageId);
+      });
     });
 
     if (messages.length === 0) {
-      const h3 = document.createElement('h3')
-      h3.textContent = 'Nenhum recado cadastrado.'
-      messagesContainer.appendChild(h3)
+      const h3 = document.createElement('h3');
+      h3.textContent = 'Nenhum recado cadastrado.';
+      messagesContainer.appendChild(h3);
     }
   } catch (error) {
-    console.log('Erro ao buscar mensagens', error)
+    console.log('Erro ao buscar mensagens', error);
   }
 }
 
-fetchMessages(currentPage)
+fetchMessages(currentPage);
 
 function navigateToEditPage(messageId) {
-  location.href = `editar-recado.html?id=${messageId}`
+  location.href = `editar-recado.html?id=${messageId}`;
 }
 
 prevPage.addEventListener('click', () => {
   if (currentPage > 1) {
-    currentPage--
-    fetchMessages(currentPage)
+    currentPage--;
+    fetchMessages(currentPage);
+    currentPageInput.value = currentPage; // Atualiza o valor do input
   }
-})
+});
 
 nextPage.addEventListener('click', () => {
   if (currentPage < totalPages) {
     currentPage++;
-    fetchMessages(currentPage)
+    fetchMessages(currentPage);
+    currentPageInput.value = currentPage; // Atualiza o valor do input
   }
-})
+});
+
+goToPageBtn.addEventListener('click', () => {
+  const requestedPage = parseInt(currentPageInput.value);
+  if (requestedPage >= 1 && requestedPage <= totalPages) {
+    currentPage = requestedPage;
+    fetchMessages(currentPage);
+  } else {
+    alert('Página inválida');
+  }
+});
